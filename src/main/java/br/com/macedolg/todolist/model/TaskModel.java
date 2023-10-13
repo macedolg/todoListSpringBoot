28bd989ac061db33ec9020e0c293fd4b2325905e
Lucas Gabriel Macêdo <lucasgabriel060405@gmail.com>
2023-10-12 21:34:53 -0300

    [Add] Criação do Modelo do Tarefas

---
+++ src/main/java/br/com/macedolg/todolist/model/TaskModel.java
@@ -0,0 +1,45 @@
+package br.com.macedolg.todolist.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+/**
+ *
+ * ID
+ * Usuário (ID_USUARIO)
+ * Descrição
+ * Título
+ * Data de Início
+ * Data de término
+ * Prioridade
+ *
+ */
+
+@Data
+@Entity(name ="tbTask")
+public class TaskModel {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    public UUID id;
+    public String description;
+
+    @Column(length = 50)
+    public String tittle;
+    public LocalDateTime startAt;
+    public LocalDateTime endAt;
+    public String priority;
+
+    public UUID idUser;
+
+    @CreationTimestamp
+    public LocalDateTime createdAt;
+
+}
