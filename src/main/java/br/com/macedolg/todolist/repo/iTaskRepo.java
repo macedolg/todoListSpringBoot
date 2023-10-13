7f5edc4626c7dc07852872d3c2a1346a4c8e2597
Lucas Gabriel Macêdo <lucasgabriel060405@gmail.com>
2023-10-12 21:35:19 -0300

    [Add] Criação do Repositorio do Tarefas

---
+++ src/main/java/br/com/macedolg/todolist/repo/iTaskRepo.java
@@ -0,0 +1,12 @@
+package br.com.macedolg.todolist.repo;
+
+import br.com.macedolg.todolist.model.TaskModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface iTaskRepo extends JpaRepository<TaskModel, UUID> {
+    List<TaskModel> findByIdUser(UUID idUser);
+
+}
