e1c2348dc3415ef7ad352f4bd0bc94ea68663d80
Lucas Gabriel Macêdo <lucasgabriel060405@gmail.com>
2023-10-13 14:28:40 -0300

    [Add] ExceptionHandler global para tratamento de erros

---
+++ src/main/java/br/com/macedolg/todolist/errors/ExceptionHandlerController.java
@@ -0,0 +1,16 @@
+package br.com.macedolg.todolist.errors;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionHandlerController {
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<String> handleHttpMessageNotReadableException(HttpMessageNotReadableException notReadableException) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(notReadableException.getMostSpecificCause().getMessage());
+    }
+}
